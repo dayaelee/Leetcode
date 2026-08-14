33183f500babb5d2c2303de1d6b1eda5b1a4b3c3
horan482 <dayae.dev@gmail.com>
2026-08-14 23:57:25 +0900

    [solved] easy 3090. Maximum Length Substring With Two Occurrences
    Implement a method to find the maximum length of a substring containing at most two distinct characters.

--- 3090. Maximum Length Substring With Two Occurrences.py
+++ 3090. Maximum Length Substring With Two Occurrences.py
@@ -1,1 +1,29 @@
-ongoing
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+
+        l, r = 0, -1
+
+        tmp = defaultdict(int)
+        answer = 0
+        cnt = 0
+        while l<len(s):
+            # print()
+            while r+1<len(s):
+                items = s[r+1]
+                if tmp[items]+1<=2:
+                    # print('where are you: ', r+1)
+                    tmp[items]+=1
+                    cnt+=1
+                    r+=1
+                else:
+                    break
+                
+            # print('cnt: ', cnt)
+            answer=max(answer, cnt)
+            cnt-=1
+            item = s[l]
+            tmp[item]-=1
+            l+=1
+        # print(answer)
+        return answer
+        
